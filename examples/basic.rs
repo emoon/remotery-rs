b821915f2089035a59a1b31b6883025830c40b78
Daniel Collin <daniel@collin.com>
2016-07-24 16:57:16 +0200

    Sample code

--- examples/basic.rs
+++ examples/basic.rs
@@ -1,32 +1,25 @@
 extern crate remotery;
 
-use remotery::{Remotery, RemoteryScope};
+use remotery::{Remotery, RemoteryScope, SampleFlags};
 use std::time::Duration;
 use std::thread;
 
 fn some_function() {
-    let _scope = RemoteryScope::new("some_function", 0);
-    thread::sleep(Duration::from_millis(100));
+    let _scope = RemoteryScope::new("some_function", SampleFlags::Default);
+    thread::sleep(Duration::from_millis(10));
 }
 
 fn main() {
-    println!("start");
-    let _remotery = match Remotery::create_global_instance() {
-        Ok(inst) => inst,
-        Err(e) => {
-            println!("Unable to Create Remotery: {}", e);
-            return;
-        }
-    };
+    let _remotery = Remotery::create_global_instance().unwrap_or_else(|e| {
+    	panic!(e);
+	});
 
-    println!("loop");
-    for _ in 0..100 {
+    for _ in 0..1000 {
         Remotery::log_text("Doing profiling!");
-        Remotery::begin_cpu_sample("test", 0);
-        thread::sleep(Duration::from_millis(100));
-        Remotery::end_cpu_sample();
+        Remotery::begin_cpu_sample("test", SampleFlags::Default);
+        thread::sleep(Duration::from_millis(20));
         some_function();
+        Remotery::end_cpu_sample();
     }
-    println!("end");
 }
 
